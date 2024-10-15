5b8e121d096a9335914f69c8f8ff6a2f2ec4aebb
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2024-10-15 18:32:40 +0700

    Create 0.9 Primitive vs Abstract.java

---
+++ Pertemuan 0/0.9 Primitive vs Abstract.java
@@ -0,0 +1,22 @@
+package bnccsesi0;
+
+public class BNCCSesi0 {
+
+    public static void main(String[] args) {
+        /* Primitive vs Abstract data type
+        Abstract data type are object variant of the primitive data type
+        Abstract data type are usually start with uppercase
+        */
+        Integer x; // int
+        Double d;  // double
+        Boolean b; // boolean
+        String s;  // string (there is no primtive version of string)
+        
+        System.out.println(Integer.parseInt("8932")); 
+        // Abstract data type has some built in function that can be helpful, such as, converting String to int
+    } 
+    
+}
+/* Program output
+8932
+*/
