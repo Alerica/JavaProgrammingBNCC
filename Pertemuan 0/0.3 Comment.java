c4e56baac59310bf138cb6bf5f529384e102b6a2
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2024-10-15 17:49:06 +0700

    Create 0.3 Comment.java

---
+++ Pertemuan 0/0.3 Comment.java
@@ -0,0 +1,18 @@
+package bnccsesi0;
+
+public class BNCCSesi0 {
+
+    public static void main(String[] args) {
+        // This is a comment
+        /*
+        This
+        is 
+        a
+        multi-line
+        comment
+        */
+        
+        // Comment doesn`t effect the output; it is used to give the programmer or other programmer what specific code does.
+    }
+    
+}
