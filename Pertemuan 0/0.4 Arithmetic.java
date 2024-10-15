a3d9adf5f2333a817db67a648b9ec316f070163d
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2024-10-15 17:52:57 +0700

    Create 0.4 Arithmetic.java

---
+++ Pertemuan 0/0.4 Arithmetic.java
@@ -0,0 +1,24 @@
+package bnccsesi0;
+
+public class BNCCSesi0 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        
+        System.out.println("a + b is : " + (a + b));
+        System.out.println("a - b is : " + (a - b));
+        System.out.println("a * b is : " + (a * b));
+        System.out.println("a / b is : " + (a / b));
+        System.out.println("a % b is : " + (a % b)); // 10 mod 3 have a remainder of 1, hence the output is 1
+    }
+    
+}
+
+/* Output of the Program
+a + b is : 13
+a - b is : 7
+a * b is : 30
+a / b is : 3
+a % b is : 1
+*/
