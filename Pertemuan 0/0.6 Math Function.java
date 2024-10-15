704272083c1d302117d62a9f28f023fa9718452d
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2024-10-15 18:07:19 +0700

    Create 0.6 Math Function.java

---
+++ Pertemuan 0/0.6 Math Function.java
@@ -0,0 +1,24 @@
+package bnccsesi0;
+
+public class BNCCSesi0 {
+
+    public static void main(String[] args) {
+        System.out.println(Math.max(7, 9)); // Find the maximum value
+        System.out.println(Math.min(7, 9)); // Find the minimum Value
+        System.out.println(Math.pow(2,2)); // Find the power of x^y (x,y)
+        System.out.println(Math.sqrt(9)); // Find the square root of x
+        System.out.println(Math.ceil(3.1)); // Round down
+        System.out.println(Math.floor(3.9)); // Round up
+        System.out.println(Math.round(3.5)); // Round to the nearest round value
+    }
+    
+}
+/* Program output
+9
+7
+4.0
+3.0
+4.0
+3.0
+4
+*/
