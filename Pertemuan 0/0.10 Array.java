92ee951ca85663df3d594f590fe23bd7b3053353
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2024-10-15 18:37:02 +0700

    Create 0.10 Array.java

---
+++ Pertemuan 0/0.10 Array.java
@@ -0,0 +1,17 @@
+package bnccsesi0;
+
+public class BNCCSesi0 {
+
+    public static void main(String[] args) {
+        // This is an array, it can contain multiple value of the same data type
+        int a[] = {4,5,6,7,3,5,9,3,4};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("");
+    } 
+    
+}
+/* Program output
+4 5 6 7 3 5 9 3 4 
+*/
