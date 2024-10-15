1f485e32ea7e54add3a87d7642cf586f4083109d
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2024-10-15 18:11:12 +0700

    Create 0.7 Conditional (if).java

---
+++ Pertemuan 0/0.7 Conditional (if).java
@@ -0,0 +1,30 @@
+package bnccsesi0;
+
+public class BNCCSesi0 {
+
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("Do this");
+        }
+        
+        if (false) {
+            System.out.println("It don`t do this");
+        } else {
+            System.out.println("It will do this instead");
+        }
+        
+        if(false) {
+            System.out.println("First choice");
+        } else if (true) {
+            System.out.println("Second choice");
+        } else {
+            System.out.println("Third choice");
+        }
+    } 
+    
+}
+/* Program output
+Do this
+It will do this instead
+Second choice
+*/
